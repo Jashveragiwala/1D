94086a07d848c4054bd5416dda7a82556e693f23
exit <user.exit>
2023-04-01 19:27:14 +0800

    1/4/23 (added scroll feature)

--- app/src/main/java/com/example/a1d/All_JourneysActivity.java
+++ app/src/main/java/com/example/a1d/All_JourneysActivity.java
@@ -9,10 +9,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -24,25 +24,14 @@ import com.google.android.material.button.MaterialButton;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class All_JourneysActivity extends AppCompatActivity {
 
     private LinearLayout buttonContainer;
-    private int buttonCount = 1;
-
     Button ButtonC;
     TextView txtView;
-
-    // sharedPreferences
-    // GSON is a library that will convert java objects into JSON format
-    // we can use this to store our JourneyObjects
-    public static void saveObjectToSharedPreference(Context context, String preferenceFileName, String serializedObjectKey, Object object) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences(preferenceFileName, 0);
-        SharedPreferences.Editor sharedPreferencesEditor = sharedPreferences.edit();
-        final Gson gson = new Gson();
-        String serializedObject = gson.toJson(object);
-        sharedPreferencesEditor.putString(serializedObjectKey, serializedObject);
-        sharedPreferencesEditor.apply();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,15 +44,6 @@ public class All_JourneysActivity extends AppCompatActivity {
         txtView = (TextView) findViewById(R.id.All_journeys_heading);
 
         ButtonC = (Button) findViewById(R.id.journeys3);
-//        ButtonC.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view){
-//                String TAG = "HomePage";
-//                Log.d(TAG, "onClick: get started is clicked"); // logcat logs that button is clicked
-//                Intent intent = new Intent(All_JourneysActivity.this, MainActivity3.class);
-//                startActivity(intent);
-//            }}
-//        );
 
         Button testButton = findViewById(R.id.test_maps_button);
         testButton.setOnClickListener(new View.OnClickListener(){
@@ -74,7 +54,15 @@ public class All_JourneysActivity extends AppCompatActivity {
             }
         });
 
-
+//        ButtonC.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view){
+//                String TAG = "HomePage";
+//                Log.d(TAG, "onClick: get started is clicked"); // logcat logs that button is clicked
+//                Intent intent = new Intent(All_JourneysActivity.this, MainActivity3.class);
+//                startActivity(intent);
+//            }}
+//        );
 
         NavigationBarView bottomNav = findViewById(R.id.bottom_nav);
         bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
@@ -97,65 +85,56 @@ public class All_JourneysActivity extends AppCompatActivity {
             }
         });
     }
+    private int buttonCount = 1;
+    public void addButton(View view) {
+        buttonContainer = findViewById(R.id.button_container);
+
+        MaterialButton newButton = new MaterialButton(this);
+        newButton.setId(View.generateViewId());
+        System.out.println(newButton.getId());
+
+        ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(90, 20, 90, 0);
+        newButton.setLayoutParams(params);
+        // Set the button's corner radius and background color
+        newButton.setCornerRadius(60);
+        newButton.setBackgroundColor(Color.WHITE);
+        // Set the button's height
+        newButton.setHeight(300);
+        // Set the button's padding
+        newButton.setPadding(90, 50, 0, 50);
+        newButton.setText("Journey");
+        newButton.setTextColor(Color.parseColor("#18C0C1"));
+        newButton.setTextSize(45);
+        newButton.setIcon(getResources().getDrawable(R.drawable.trash));
+        newButton.setIconSize(100); // reduce the icon size
+        newButton.setIconTint(ColorStateList.valueOf(Color.parseColor("#FF0000")));
+        Drawable icon = getResources().getDrawable(R.drawable.trash);
+        icon.setBounds(0, 0, 100, 100);
+        newButton.setCompoundDrawables(icon, null, null, null);
+
+        // Set OnClickListener on the button
+        LinearLayout layout = findViewById(R.id.button_container);
+        layout.addView(newButton);
 
 
-        public void addButton(View view) {
-            buttonContainer = findViewById(R.id.button_container);
-            if (buttonCount <= 4) {
-                MaterialButton newButton = new MaterialButton(this);
-                newButton.setId(View.generateViewId());
-                newButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(All_JourneysActivity.this, MainActivity4.class);
-                        startActivity(intent);
-                    }
-                });
-
-                ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(
-                        ConstraintLayout.LayoutParams.MATCH_PARENT,
-                        ConstraintLayout.LayoutParams.WRAP_CONTENT
-                );
-                params.setMargins(90, 20, 90, 0);
-                newButton.setLayoutParams(params);
-
-                // Set the button's corner radius and background color
-                newButton.setCornerRadius(60);
-                newButton.setBackgroundColor(Color.WHITE);
-
-                // Set the button's height
-                newButton.setHeight(300);
-
-                // Set the button's padding
-                newButton.setPadding(90, 50, 0, 50);
-
-                newButton.setText("Day " + buttonCount);
-                newButton.setTextColor(Color.parseColor("#18C0C1"));
-                newButton.setTextSize(45);
-
-                newButton.setIcon(getResources().getDrawable(R.drawable.trash));
-                newButton.setIconSize(140);
-                newButton.setIconTint(ColorStateList.valueOf(Color.parseColor("#FF0000")));
-
-                // Add the button to the layout
-                LinearLayout layout = findViewById(R.id.button_container);
-                layout.addView(newButton);
-
-                buttonCount++;
-
-            }
-            else if (buttonCount == 5) {
-                TextView textView = new TextView(this);
-                textView.setText("Only 4 journeys are allowed.");
-                textView.setTextColor(Color.RED);
-                textView.setGravity(Gravity.CENTER);
-                buttonContainer.addView(textView);
-                buttonCount++;
-            }
-            else {
-                buttonCount++;
-            }
-        }
+        buttonCount++;
+        newButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int x = (int) event.getX();
+                if (x < 220) {
+                    // Delete the button
+                    layout.removeView(newButton);
+                    buttonCount--;
+                } else {
+                    // Start the new activity
+                    Intent intent = new Intent(All_JourneysActivity.this, MainActivity4.class);
+                    startActivity(intent);
+                }
+                return true;
+            }});
+    }
 
     private void requetsWindowFeature(int featureNoTitle) {
     }
